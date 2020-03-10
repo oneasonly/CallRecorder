a1bc7939ed9407ffe9aa54fd2cb38a67231768db
workPC <asifgeass@gmail.com>
2020-03-10 14:40:34 +0300

    second version after office work 10.03

--- UIWpf/MainWindow.xaml.cs
+++ UIWpf/MainWindow.xaml.cs
@@ -27,12 +27,40 @@ namespace UIWpf
         WasapiCapture CaptureInstance;
         WaveOut _audioPlayer = new WaveOut();
         int count = 1;
+        bool isLetClick = true;
+        private readonly string recordDir = @"records";
         #endregion
 
         #region Private Methods
         private void OnLoad()
         {
-            RecordStopButton.IsEnabled = false;
+            CallInputTextBox.Focus();
+            //RecordStopButton.IsEnabled = false;
+            _audioPlayer.PlaybackStopped += audioPlayer_PlaybackStopped;
+        }
+
+        private async void audioPlayer_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            await Task.Delay(350);            
+            SetIconCallStart();
+        }
+
+        public async Task AppMonitoring()
+        {
+            bool prevCheck = false;
+
+            while (true)
+            {
+                await Task.Delay(1000);
+
+                bool isAudioPlaying = _audioPlayer.PlaybackState == PlaybackState.Playing;
+
+                if (isAudioPlaying != prevCheck)
+                {
+                    
+                }
+                prevCheck = isAudioPlaying;
+            }
         }
 
         private void RecordStart(string filename)
@@ -61,9 +89,13 @@ namespace UIWpf
 
         private void RecordStart()
         {
+            if(!Directory.Exists(recordDir))
+            {
+                Directory.CreateDirectory(recordDir);
+            }
             string patern = "yyyy.MM.dd hh-mm";
             string endFile = ".wav";
-            string filename = $@"records\mic {DateTime.Now.ToString(patern)}";
+            string filename = $@"{recordDir}\{CallInputTextBox.Text} {DateTime.Now.ToString(patern)}";
             string fullFilename = filename + endFile;
             while (File.Exists(fullFilename))
             {
@@ -88,27 +120,72 @@ namespace UIWpf
             waveOut.Play();
         }
 
-        private void PlayAudio()
-        {
-            PlayAudio("The Score - The Fear.mp3"); //The Score - The Fear.mp3 //call 2020.03.08 09-40.wav
-        }
-        private void PlayAudio(string filename)
+        private async void PlayAudio(string filename)
         {
             if(_audioPlayer.PlaybackState == PlaybackState.Playing)
             {
-                _audioPlayer.Pause();
+                _audioPlayer.Stop();
             }
-            else if (_audioPlayer.PlaybackState == PlaybackState.Paused)
+            if (_audioPlayer.PlaybackState == PlaybackState.Paused)
             {
-                _audioPlayer.Resume();
+                _audioPlayer.Stop();
             }
+
+            var reader = new AudioFileReader(filename);
+
+            
+            _audioPlayer.Init(reader);
+            await Task.Delay(500);
+            _audioPlayer.Play();
+
+            //var reader = new AudioFileReader(filename); //The Score - The Fear.mp3 //call 2020.03.08 09-40.wav
+            //_audioPlayer = new WaveOut(); // or WaveOutEvent()
+        }
+
+        private string GetFilenameByNumber()
+        {
+            string fullFilePath = null;
+            string fileName = null;
+            if (CallInputTextBox.Text == "101" || CallInputTextBox.Text == "112")
+            {
+                fileName = @"101.ogg";
+            }
+            else if (CallInputTextBox.Text == "102")
+            {
+                fileName = @"Fight the fear.mp3";
+            }
+            else if (CallInputTextBox.Text == "103")
+            {
+
+            }
+            else if (CallInputTextBox.Text == "104")
+            {
+
+            }
+            if (fileName == null) return null;
+            fullFilePath = $@"rules\{fileName}";
+            return fullFilePath;
+        }
+
+        private async Task ChangeIconCall()
+        {
+            await Task.Delay(320);
+            if (CallIcon.Kind == MaterialDesignThemes.Wpf.PackIconKind.Call)
+                SetIconCallStop();
             else
-            {
-                var reader = new AudioFileReader(filename); //The Score - The Fear.mp3 //call 2020.03.08 09-40.wav
-                //_audioPlayer = new WaveOut(); // or WaveOutEvent()
-                _audioPlayer.Init(reader);
-                _audioPlayer.Play();
-            }
+                SetIconCallStart();
+        }
+
+        private void SetIconCallStop()
+        {
+            CallIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.CallEnd;
+            CallIcon.Foreground = Brushes.OrangeRed;
+        }
+
+        private void SetIconCallStart()
+        {
+            CallIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.Call;
+            CallIcon.Foreground = Brushes.LawnGreen;
         }
         #endregion
 
@@ -121,27 +198,52 @@ namespace UIWpf
 
         private void RecordStartButton_Click(object sender, RoutedEventArgs e)
         {
-            RecordStartButton.IsEnabled = false;
-            RecordStopButton.IsEnabled = true;
+            //RecordStartButton.IsEnabled = false;
+            //RecordStopButton.IsEnabled = true;
             RecordStart();
         }
 
         private void RecordStopButton_Click(object sender, RoutedEventArgs e)
         {
-            RecordStopButton.IsEnabled = false;
-            RecordStartButton.IsEnabled = true;
+            //RecordStopButton.IsEnabled = false;
+            //RecordStartButton.IsEnabled = true;
             RecordStop();
         }
-        #endregion
 
         private void PlayWavButton_Click(object sender, RoutedEventArgs e)
         {
-            PlayAudio();
+            PlayAudio("The Score - The Fear.mp3"); //The Score - The Fear.mp3 //call 2020.03.08 09-40.wav
         }
 
         private void PlayMp3Button_Click(object sender, RoutedEventArgs e)
         {
             PlayMP3();
         }
+
+        private async void CallKeyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isLetClick)
+            {
+                isLetClick = false;
+                if(CallIcon.Kind == MaterialDesignThemes.Wpf.PackIconKind.CallEnd)
+                {
+                    _audioPlayer.Stop();
+                }
+                else
+                {
+                    string filePath = GetFilenameByNumber();
+                    if (filePath != null)
+                    {
+                        PlayAudio(filePath);
+                        RecordStart();
+                        ChangeIconCall();
+                    }                    
+                }
+                await Task.Delay(350);
+                isLetClick = true;
+            }
+        }
+        #endregion
+
     }
 }
